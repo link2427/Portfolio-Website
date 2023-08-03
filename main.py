c238a18a7a752fb5bcb5fd7259f84ddbfaa35d67
Jacob <62169030+link2427@users.noreply.github.com>
2023-08-03 15:51:16 -0500

    made everything local

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/null')
+def null():
+    return render_template('null.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
